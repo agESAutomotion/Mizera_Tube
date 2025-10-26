8be942fc0a90302f6b55f31c9774de973e192584
Adrián González <adrian.gonzalez@esautomotion.com>
2025-10-26 14:01:43 +0100

    Convert absolute paths to relative paths

--- EsaRun.bat
+++ EsaRun.bat
@@ -1,11 +1,11 @@
 ECHO OFF
 ping 1.1.1.1 -n 1 -w 1000 > NUL
-cd C:\Kvara\exe
-cmd /c start c:\kvara\exe\kvisogui.exe
+cd exe
+cmd /c start .\kvisogui.exe
 ping 1.1.1.1 -n 3 -w 1000 > NUL
-//cmd /c start tkskey.exe c:\kvara\plc\_key\vrtc5.key
-//cmd /c start tkskey.exe c:\kvara\plc\_key\ESAtest.key
-//cmd /c start tkskey.exe c:\kvara\plc\_key\nlightIO.key
-cmd /c start tkskey.exe c:\kvara\plc\_key\usrLogicIO.key
-cmd /c start tkskey.exe c:\kvara\plc\_key\LogicIO.key
-//cmd /c start tkskey.exe c:\kvara\plc\_key\caphead.key
+@REM start tkskey.exe ..\plc\_key\vrtc5.key
+@REM start tkskey.exe ..\plc\_key\ESAtest.key
+@REM start tkskey.exe ..\plc\_key\nlightIO.key
+start tkskey.exe ..\plc\_key\usrLogicIO.key
+start tkskey.exe ..\plc\_key\LogicIO.key
+start tkskey.exe ..\plc\_key\caphead.key
